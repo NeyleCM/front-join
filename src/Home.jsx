82b111949425a392275b1c9ee6d3302120d594c3
NeyleCM <neylecmorantes@gmail.com>
2024-11-21 10:08:30 +0100

    'conectada-con-back'

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,16 @@
-const Home = () => {
+import {Link} from 'react-router-dom'
+
+const Home = ({data}) => {
   return (
     <>
-    
+    <h2>Lista de Datos</h2>
+      <ul>
+        {data.map(item => (
+          <li key={item._id}>
+            <Link to={`/${item._id}`}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 };
